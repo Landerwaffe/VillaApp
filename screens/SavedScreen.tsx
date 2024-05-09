9c239035787bb9450fb888176fae1ad13a66c698
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-09 23:27:49 +0700

    Removed Screens

--- screens/SavedScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export const SavedScreen = () => {
-  return (
-    <View>
-      <Text>Saved Screen</Text>
-    </View>
-  );
-};
--- screens/SearchScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export const SearchScreen = () => {
-  return (
-    <View>
-      <Text>Search Screen</Text>
-    </View>
-  );
-};
