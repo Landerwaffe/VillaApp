dcd98c177ba351db5f15dd27de8502881cf0ff30
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-10 10:17:19 +0700

    Javascript inline styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,27 @@
-import { View, Text } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+
+function HomeScreen() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      style={{ flex: 1, paddingTop: insets.top, margin: "auto", width: "50%" }}
+    >
+      <Text style={{ fontFamily: "Inter-Black", fontSize: 30 }}>
+        Content is in safe area.
+      </Text>
+      <Text>Text</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View>
-        <Text style={{ fontFamily: "Inter-Black", fontSize: 30 }}>
-          Inter Black Font
-        </Text>
-      </View>
+      <HomeScreen />
     </SafeAreaProvider>
   );
 }
