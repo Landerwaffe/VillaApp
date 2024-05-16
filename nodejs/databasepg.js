8d8e1b27ace15690865dd0544249e09c22d54d7e
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-17 00:23:06 +0700

    Function write

--- nodejs/databasepg.js
+++ nodejs/databasepg.js
@@ -16,3 +16,14 @@ client.query(`Select * from properties`, (err, res) => {
   }
   client.end;
 });
+
+export function queryDB(query) {
+  client.query(query, (err, res) => {
+    if (!err) {
+      console.log(res.rows);
+    } else {
+      console.log(err.message);
+    }
+    return res.rows;
+  });
+}
