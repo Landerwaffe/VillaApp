7d7aa52c3b61e1eed5eed25930b3d4da56915b51
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-17 00:08:49 +0700

    Select query successfully displays

---
+++ nodejs/databasepg.js
@@ -0,0 +1,18 @@
+const { Client } = require("pg");
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Villas",
+  password: "admin",
+  port: 5432,
+});
+client.connect();
+
+client.query(`Select * from properties`, (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
+  client.end;
+});
