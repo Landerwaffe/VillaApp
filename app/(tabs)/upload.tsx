6f3f28bbab42d57c1bb755d0fb10fbf9e886eb71
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-21 23:57:44 +0700

    Reconfiguring form from native form builder

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -1,101 +1,104 @@
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  StyleSheet,
-  Text,
-  StatusBar,
-} from "react-native";
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import React from "react";
+import { View, StyleSheet, ScrollView, Text } from "react-native";
+import { FormBuilder } from "react-native-paper-form-builder";
+import { useForm } from "react-hook-form";
+import { Button, DefaultTheme, useTheme } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemedText } from "@/components/ThemedText";
-import { useTheme } from "react-native-paper";
-import * as React from "react";
-import { TextInput } from "react-native-paper";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
-
-function HomeScreen() {
-  const insets = useSafeAreaInsets();
+function UploadScreen() {
   const theme = useTheme();
-  const [text, setText] = React.useState("");
-  const [text1, setText1] = React.useState("");
-  const [text2, setText2] = React.useState("");
-  const [text3, setText3] = React.useState("");
 
-  console.log("Text is: " + text);
-  console.log(setText);
-
-  const result = fetch("http://192.168.1.15:8080", { method: "POST" });
+  const styles = StyleSheet.create({
+    containerStyle: {
+      flex: 1,
+    },
+    scrollViewStyle: {
+      flex: 1,
+      padding: 15,
+      backgroundColor: theme.colors.secondary,
+      //justifyContent: "center",
+    },
+    headingStyle: {
+      fontSize: 30,
+      textAlign: "center",
+      marginBottom: 40,
+    },
+  });
+  const { control, setFocus, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        paddingTop: insets.top,
-        margin: "auto",
-        backgroundColor: theme.colors.primary,
-        width: "100%",
-      }}
-    >
-      <ThemedText
-        style={{
-          fontFamily: "Inter-Black",
-          fontSize: 30,
-          paddingTop: "3%",
-          paddingLeft: "8.4%",
-        }}
-      >
-        Uploading a listing.
-      </ThemedText>
-      <ThemedText
-        style={{
-          fontFamily: "Inter-Regular",
-          fontSize: 25,
-          paddingLeft: "8.4%",
-        }}
-      >
-        Taking user input
-      </ThemedText>
-      <TextInput
-        label="Name"
-        value={text}
-        onChangeText={(text) => setText(text)}
-        style={{
-          marginTop: "8.4%",
-        }}
-      />
-      <TextInput
-        label="Subtitle"
-        value={text1}
-        onChangeText={(text1) => setText1(text1)}
-      />
-      <TextInput
-        label="Image"
-        value={text2}
-        onChangeText={(text2) => setText2(text2)}
-      />
-      <TextInput
-        label="Description"
-        value={text3}
-        onChangeText={(text3) => setText3(text3)}
-      />
+    <View style={styles.containerStyle}>
+      <ScrollView contentContainerStyle={styles.scrollViewStyle}>
+        <ThemedText
+          style={{
+            fontFamily: "Inter-Black",
+            fontSize: 30,
+            paddingTop: "3%",
+            textAlign: "center",
+          }}
+        >
+          Adding a new listing.
+        </ThemedText>
+        <ThemedText
+          style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 25,
+            paddingTop: "0.5%",
+            textAlign: "center",
+          }}
+        >
+          Taking user input
+        </ThemedText>
+        <FormBuilder
+          control={control}
+          setFocus={setFocus}
+          formConfigArray={[
+            {
+              type: "email",
+              name: "email",
+
+              rules: {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+              },
+              textInputProps: {
+                label: "Email",
+              },
+            },
+            {
+              type: "password",
+              name: "password",
+              rules: {
+                required: {
+                  value: true,
+                  message: "Password is required",
+                },
+              },
+              textInputProps: {
+                label: "Password",
+              },
+            },
+          ]}
+        />
+        <Button
+          mode={"contained"}
+          onPress={handleSubmit((data: any) => {
+            console.log("form data", data);
+          })}
+          style={{ backgroundColor: darktheme.colors.secondary }}
+        >
+          Submit
+        </Button>
+      </ScrollView>
     </View>
   );
 }
@@ -104,7 +107,101 @@ export default function App() {
   const theme = useTheme();
   return (
     <SafeAreaProvider>
-      <HomeScreen />
+      <UploadScreen />
     </SafeAreaProvider>
   );
 }
+
+const darktheme = {
+  ...DefaultTheme,
+  dark: true,
+  colors: {
+    primary: "rgb(79, 216, 235)",
+    onPrimary: "rgb(0, 54, 61)",
+    primaryContainer: "rgb(0, 79, 88)",
+    onPrimaryContainer: "rgb(151, 240, 255)",
+    secondary: "rgb(161, 201, 255)",
+    onSecondary: "rgb(0, 50, 91)",
+    secondaryContainer: "rgb(0, 72, 128)",
+    onSecondaryContainer: "rgb(211, 228, 255)",
+    tertiary: "rgb(133, 207, 255)",
+    onTertiary: "rgb(0, 52, 76)",
+    tertiaryContainer: "rgb(0, 76, 108)",
+    onTertiaryContainer: "rgb(199, 231, 255)",
+    error: "rgb(255, 180, 171)",
+    onError: "rgb(105, 0, 5)",
+    errorContainer: "rgb(147, 0, 10)",
+    onErrorContainer: "rgb(255, 180, 171)",
+    background: "rgb(25, 28, 29)",
+    onBackground: "rgb(225, 227, 227)",
+    surface: "rgb(25, 28, 29)",
+    onSurface: "rgb(225, 227, 227)",
+    surfaceVariant: "rgb(63, 72, 74)",
+    onSurfaceVariant: "rgb(191, 200, 202)",
+    outline: "rgb(137, 146, 148)",
+    outlineVariant: "rgb(63, 72, 74)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(225, 227, 227)",
+    inverseOnSurface: "rgb(46, 49, 50)",
+    inversePrimary: "rgb(0, 104, 116)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(28, 37, 39)",
+      level2: "rgb(29, 43, 46)",
+      level3: "rgb(31, 49, 52)",
+      level4: "rgb(32, 51, 54)",
+      level5: "rgb(33, 54, 58)",
+    },
+    surfaceDisabled: "rgba(225, 227, 227, 0.12)",
+    onSurfaceDisabled: "rgba(225, 227, 227, 0.38)",
+    backdrop: "rgba(41, 50, 52, 0.4)",
+  },
+};
+
+const lighttheme = {
+  ...DefaultTheme,
+  dark: false,
+  colors: {
+    primary: "tomato",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(151, 240, 255)",
+    onPrimaryContainer: "rgb(0, 31, 36)",
+    secondary: "rgb(17, 96, 164)",
+    onSecondary: "rgb(255, 255, 255)",
+    secondaryContainer: "rgb(211, 228, 255)",
+    onSecondaryContainer: "rgb(0, 28, 56)",
+    tertiary: "rgb(0, 101, 143)",
+    onTertiary: "rgb(255, 255, 255)",
+    tertiaryContainer: "rgb(199, 231, 255)",
+    onTertiaryContainer: "rgb(0, 30, 46)",
+    error: "rgb(186, 26, 26)",
+    onError: "rgb(255, 255, 255)",
+    errorContainer: "rgb(255, 218, 214)",
+    onErrorContainer: "rgb(65, 0, 2)",
+    background: "rgb(250, 253, 253)",
+    onBackground: "rgb(25, 28, 29)",
+    surface: "rgb(250, 253, 253)",
+    onSurface: "rgb(25, 28, 29)",
+    surfaceVariant: "rgb(219, 228, 230)",
+    onSurfaceVariant: "rgb(63, 72, 74)",
+    outline: "rgb(111, 121, 122)",
+    outlineVariant: "rgb(191, 200, 202)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(46, 49, 50)",
+    inverseOnSurface: "rgb(239, 241, 241)",
+    inversePrimary: "rgb(79, 216, 235)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(238, 246, 246)",
+      level2: "rgb(230, 241, 242)",
+      level3: "rgb(223, 237, 238)",
+      level4: "rgb(220, 235, 237)",
+      level5: "rgb(215, 232, 234)",
+    },
+    surfaceDisabled: "rgba(25, 28, 29, 0.12)",
+    onSurfaceDisabled: "rgba(25, 28, 29, 0.38)",
+    backdrop: "rgba(41, 50, 52, 0.4)",
+  },
+};
