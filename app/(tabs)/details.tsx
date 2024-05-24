5ff8a86cb27f35de2f28ad7fda5308a401dcb089
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-05-24 11:36:53 +0700

    Standardized styling

--- app/(tabs)/details.tsx
+++ app/(tabs)/details.tsx
@@ -1,3 +1,29 @@
+import { View } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+
 export default function App() {
-  return <h1>This is details in tabs.</h1>;
+  return (
+    <View style={{ backgroundColor: "tomato", flex: 1 }}>
+      <ThemedText
+        style={{
+          fontFamily: "Inter-Black",
+          fontSize: 30,
+          paddingTop: "3%",
+          textAlign: "center",
+        }}
+      >
+        This is details.
+      </ThemedText>
+      <ThemedText
+        style={{
+          fontFamily: "Inter-Regular",
+          fontSize: 25,
+          paddingTop: "0.5%",
+          textAlign: "center",
+        }}
+      >
+        Nitty gritty
+      </ThemedText>
+    </View>
+  );
 }
